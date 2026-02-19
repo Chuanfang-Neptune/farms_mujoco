020aaf41e32bdc0baafd4c5b5bbdb4184c7e6d9a
Chuanfang Ning <ningchuanfang@hotmail.com>
2026-02-19 17:57:46 +0100

    [PROJECT] Suppress farms_muscle warning

--- farms_mujoco/sensors/sensors.pyx
+++ farms_mujoco/sensors/sensors.pyx
@@ -3,10 +3,11 @@
 import numpy as np
 cimport numpy as np
 
-# try:
-#     from farms_muscle import rigid_tendon as rt
-# except:
-#     print("farms_muscle not installed")
+try:
+    from farms_muscle import rigid_tendon as rt
+except:
+    # print("farms_muscle not installed")
+    pass
 from mujoco import mj_contactForce
 
 from libc.math cimport abs, fmax, sqrt
